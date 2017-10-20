4c8fa14e71d1b8c56c48a6b3a8487a869bfa06d1
gfechio <giordano.fechio@booking.com>
2017-10-20 16:51:43 +0200

    Orchid Start

---
+++ orchid/orchid.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"github.com/fsouza/go-dockerclient"
+	"os"
+)
+
+//func check(e error) {
+//	if e != nil {
+//		panic(e)
+//	}
+//}
+
+func main() {
+	if _, err := os.Stat("webserver"); os.IsNotExist(err) {
+		fmt.Println("Creating Dockerfile")
+
+		endpoint := "unix:///var/run/docker.sock"
+		client, err := docker.NewClient(endpoint)
+		if err != nil {
+			panic(err)
+		}
+
+		imgs, err := client.ListImages(docker.ListImagesOptions{All: false})
+		if err != nil {
+			panic(err)
+		}
+
+		for _, img := range imgs {
+			fmt.Println("ID: ", img.ID)
+			fmt.Println("RepoTags: ", img.RepoTags)
+			fmt.Println("Created: ", img.Created)
+			fmt.Println("Size: ", img.Size)
+			fmt.Println("VirtualSize: ", img.VirtualSize)
+			fmt.Println("ParentId: ", img.ParentID)
+		}
+	}
+}
