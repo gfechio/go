5bb888671373c9f15a8c9396fbc143184b497f2d
gfechio <giordano.fechio@booking.com>
2017-11-16 22:52:55 +0100

    Starting run func

--- orchid/orchid.go
+++ orchid/orchid.go
@@ -1,66 +1,66 @@
 package main
 
-import ( 
-    "fmt"
-    "context"
-    "github.com/docker/docker/api/types"
-    "github.com/docker/docker/client"
-    "os"
+import (
+	"context"
+	"fmt"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"os"
 )
 
 func main() {
-//XXX: Include functions list , run (receive port range- OPTIONAL, image name), kill (image name)
-    action := os.Args[1]
-    switch action {
-    case "list":
-        list()
-    case "run":
-        run()
-    case "kill":
-        kill()
-    default:
-        fmt.Println("Arguments must be list, run or kill")
-    }
+	//XXX: Include functions list , run (receive port range- OPTIONAL, image name), kill (image name)
+	action := os.Args[1]
+	switch action {
+	case "list":
+		list()
+		//	case "run":
+		//		run()
+	case "kill":
+		kill()
+	default:
+		fmt.Println("Arguments must be list, run or kill")
+	}
 }
 
-func list(){
-    cli, err := client.NewEnvClient()
-    if err != nil {
-        panic(err)
-    }
-    containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
-    if err != nil {
-        panic(err)
-    }
+func list() {
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	if err != nil {
+		panic(err)
+	}
 
-    for _, container := range containers {
-        fmt.Printf("%s %s %s\n", container.ID[:10], container.Image, container.Ports)
-    }
+	for _, container := range containers {
+		fmt.Printf("%s %s %s\n", container.ID[:10], container.Image, container.Ports)
+	}
 }
 
 func run() {
-    image := os.Args[2]
-    ports := os.Args[3:4]
-    command := os.Args[5]
-    cli, err := client.NewEnvClient()
-    if err != nil {
-        panic(err)
-    }
+	image := os.Args[2]
+	ports := os.Args[3:4]
+	//	command := os.Args[5]
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+
+	containers := cli.ContainerStart(context.Background(), image, ports)
 }
 
 func kill() {
-    id := os.Args[2]
-    signal := os.Args[3]
+	id := os.Args[2]
+	signal := os.Args[3]
 
-    cli, err := client.NewEnvClient()
-    if err != nil {
-        panic(err)
-    }
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
 
-    containers, err := cli.ContainerKill(context.Background(), id, signal)
-    if err != nil {
-        panic(err)
-    }
+	containers := cli.ContainerKill(context.Background(), id, signal)
+	fmt.Println("Killing your container ... softly:\n")
+	fmt.Println(containers)
 
 }
-
